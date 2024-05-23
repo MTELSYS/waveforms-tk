64e92812fa29848f3cefd5e2ece2059398760d15
ipeglin <philip@eglin.no>
2024-05-23 20:31:50 +0200

    adding basic layout using flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,197 +1,256 @@
 <script lang="ts">
-  import { Dropzone, RadioButton, ButtonGroup, Card  } from 'flowbite-svelte'
-  import { FileChartBarOutline, CloseOutline  } from 'flowbite-svelte-icons'
+	import {
+		Dropzone,
+		RadioButton,
+		ButtonGroup,
+		Card,
+		Label,
+		Input,
+		NumberInput,
+		Checkbox,
+		Helper,
+		CloseButton,
+	} from 'flowbite-svelte';
+	import { FileChartBarOutline, CloseOutline, ChartOutline, ChartMixedOutline, CodeForkOutline, InfoCircleOutline } from 'flowbite-svelte-icons';
 
-  const plotTypes: string[] = ['Scope', 'Spectrum', 'Network']
-  let selectedPlotType: string =  plotTypes[0]
+	let selectedPlotType: string = 'scope';
 
-  let numberOfChannels: number = 1
-  let channelsToShow: number = 0
+	let numberOfChannels: number = 1;
+	let channelsToShow: number = 0;
 
+	let value: any = [];
+	const dropHandle = (event) => {
+		value = [];
+		event.preventDefault();
+		if (event.dataTransfer.items) {
+			const file = [...event.dataTransfer.items]
+				.filter((item, i) => item.kind === 'file' && item.type === 'text/csv')
+				.map((file) => file.getAsFile())[0];
+			if (!file) {
+				return;
+			}
+			value.push(file);
+			value = value;
+		} else {
+			[...event.dataTransfer.files].forEach((file, i) => {
+				value = file.name;
+			});
+		}
+	};
 
-  let value: any = []
-  const dropHandle = (event) => {
-    value = []
-    event.preventDefault()
-    if (event.dataTransfer.items) {
-      const file = [...event.dataTransfer.items]
-        .filter((item, i) => item.kind === 'file' && item.type === 'text/csv')
-        .map((file) => file.getAsFile())[0]
-      if (!file) {
-        return
-      }
-      value.push(file)
-      value = value
-    } else {
-      [...event.dataTransfer.files].forEach((file, i) => {
-        value = file.name
-      })
-    }
-  }
+	const handleChange = (event) => {
+		const files = event.target.files;
+		if (files.length > 0 && files[0].type === 'text/csv') {
+			value.push(files[0]);
+			value = value;
+		}
+	};
 
-  const handleChange = (event) => {
-    const files = event.target.files
-    if (files.length > 0 && files[0].type === 'text/csv') {
-      value.push(files[0])
-      value = value
-    }
-  }
-
-  const handleRemove = (filename: any) => {
-    return () => {
-      value = value.filter((f: any) => f.name !== filename)
-    }
-  }
+	const handleRemove = (filename: any) => {
+		return () => {
+			value = value.filter((f: any) => f.name !== filename);
+		};
+	};
 </script>
 
 <section id="banner">
-  <h1>Waveforms Toolkit</h1>
-  <p class="font-subtle">A tool made for easily plotting your Analog Discovery 2 data during lab sessions in ESDA</p>
+	<h1>Waveforms Toolkit</h1>
+	<p class="font-subtle">
+		A tool made for easily plotting your Analog Discovery 2 data during lab sessions in ESDA
+	</p>
 </section>
 
 <section id="uploader">
-  <h2>Upload your data </h2>
-  <p class="font-subtle">File should be CSV</p>
+	<h2>Upload your data</h2>
+	<p class="font-subtle">File should be CSV</p>
 
-  {#if value.length === 0}
-    <Dropzone
-      id="dropzone"
-      on:drop={dropHandle}
-      on:dragover={(event) => {
-        event.preventDefault()
-      }}
-      on:change={handleChange}
-    >
-      <FileChartBarOutline size="xl" class="icon-primary" />
-      <p><span class="font-semibold">Click to upload</span> or drag and drop</p>
-      <p>CSV</p>
-    </Dropzone>
-  {:else}
-    {#each value as file}
-      <Card>
-        <div class="card">
-
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <!-- svelte-ignore a11y-no-static-element-interactions -->
-          <div on:click={handleRemove(file.name)} id="close">
-            <CloseOutline/>
-          </div>
-
-          <div class="fileinfo">
-            <h2>{file.name}</h2>
-            <p>
-              {
-                file.size.toLocaleString('en-US', {
-                  notation: 'compact',
-                  compactDisplay: 'short',
-                })
-              }B
-                • {file.type}
-            </p>
-          </div>
-        </div>
-      </Card>
-    {/each}
-  {/if}
+	{#if value.length === 0}
+		<Dropzone
+			id="dropzone"
+			on:drop={dropHandle}
+			on:dragover={(event) => {
+				event.preventDefault();
+			}}
+			on:change={handleChange}
+		>
+			<FileChartBarOutline size="xl" class="icon-primary" />
+			<p><span class="font-semibold">Click to upload</span> or drag and drop</p>
+			<p>CSV</p>
+		</Dropzone>
+	{:else}
+		{#each value as file}
+			<Card>
+				<div class="card flex flex-cols justify-between">
+					<div class="fileinfo">
+						<h2>{file.name}</h2>
+						<p>
+							{file.size.toLocaleString('en-US', {
+								notation: 'compact',
+								compactDisplay: 'short'
+							})}B • {file.type}
+						</p>
+					</div>
+					<CloseButton on:click={handleRemove(file.name)} />
+				</div>
+			</Card>
+		{/each}
+	{/if}
 </section>
 
-<section id="settings">
-  <div id="settings__plot-type">
-    <h2>Plot type</h2>
-    
-    <ButtonGroup>
-      {#each plotTypes as type}
-        <RadioButton value={type} bind:group={selectedPlotType}>{type}</RadioButton>
-      {/each}
-    </ButtonGroup>
-  </div>
+<section  class="plot-type-selector flex flex-col align-center">
+		<h2>Plot type</h2>
 
-  <div id="settings__general">
-    <h2>Settings</h2>
-    <div>
-      <!-- select amount of channels in data: 1 or 2 -->
-      <label for="settings__general__channels">Number of channels</label>
-      <ButtonGroup>
-        <RadioButton value={1} bind:group={numberOfChannels}>1</RadioButton>
-        <RadioButton value={2} bind:group={numberOfChannels}>2</RadioButton>
-      </ButtonGroup>
-    </div> 
+		<ButtonGroup>
+				<RadioButton value={0} bind:group={selectedPlotType}><ChartOutline class="w-4 h-4 me-2" />Scope</RadioButton>
+				<RadioButton value={1} bind:group={selectedPlotType}><ChartMixedOutline class="w-4 h-4 me-2" />Spectrum</RadioButton>
+				<RadioButton value={2} bind:group={selectedPlotType}><CodeForkOutline class="w-4 h-4 me-2" />Network</RadioButton>
+		</ButtonGroup>
+</section>
 
-    <div>
-      <!-- select channels to plot: Both, 1 or 2 -->
-      <label for="settings__general__channel">Channel</label>
-      <ButtonGroup>
-        <RadioButton value={0} bind:group={channelsToShow}>Both</RadioButton>
-        <RadioButton value={1} bind:group={channelsToShow}>1</RadioButton>
-        <RadioButton value={2} bind:group={channelsToShow}>2</RadioButton>
-      </ButtonGroup>
-    </div>
+<section class="settings flex flex-col align-center">
+		<h2>Settings</h2>
+		<div>
+			<!-- select amount of channels in data: 1 or 2 -->
+			<label for="settings__general__channels">Number of channels</label>
+			<ButtonGroup id="settings__general__channels">
+				<RadioButton value={1} bind:group={numberOfChannels}>1</RadioButton>
+				<RadioButton value={2} bind:group={numberOfChannels}>2</RadioButton>
+			</ButtonGroup>
+		</div>
 
-    <!-- dropdown for advanced settings -->
-    <details>
-      <summary>Advanced settings</summary>
-      <div>
-        <label for="settings__general__sample-rate">Sample rate</label>
-        <input type="number" id="settings__general__sample-rate" />
-      </div>
-      <div>
-        <label for="settings__general__time-scale">Time scale</label>
-        <input type="number" id="settings__general__time-scale" />
-      </div>
-    </details>
-  </div>
-</section> 
+		{#if numberOfChannels === 2}
+		<div>
+			<!-- select channels to plot: Both, 1 or 2 -->
+			<label for="settings__general__channels-shown">Plot channels</label>
+			<ButtonGroup id="settings__general__channels-shown">
+				<RadioButton value={0} bind:group={channelsToShow}>Both</RadioButton>
+				<RadioButton value={1} bind:group={channelsToShow}>1</RadioButton>
+				<RadioButton value={2} bind:group={channelsToShow}>2</RadioButton>
+			</ButtonGroup>
+		</div>
+		{/if}
+
+		<!-- dropdown for advanced settings -->
+		<details>
+			<summary>Advanced settings</summary>
+		</details>
+		<section class="settings__category">
+			<h3 aria-describedby="helper-axis-limits">Axis Limits</h3>
+			<Helper id="helper-axis-limits">Limit the channel axis' to an interval</Helper>
+
+			<div class="settings__row">
+				<Label for="ax-x-lim" class="mb-2">Limit x-axis</Label>
+				<NumberInput id="x-lim" placeholder="min" /> -
+				<NumberInput id="x-lim" placeholder="max" />
+			</div>
+			<div class="settings__row">
+				<Label for="ax-y-lim" class="mb-2">Limit y-axis</Label>
+				<NumberInput id="y-lim" placeholder="min" /> -
+				<NumberInput id="y-lim" placeholder="max" />
+			</div>
+		</section>
+
+		<section class="settings__category">
+			<h3 aria-describedby="helper-axis-labels">Labels</h3>
+			<Helper id="helper-axis-labels">Set the channels' axis labels</Helper>
+
+			<div id="ch1-labels">
+				<h4>Channel 1</h4>
+
+				<div class="settings__row">
+					<Label for="x-label" class="mb-2 block">X-axis Label</Label>
+					<Input id="x-label" name="x-label" required placeholder="Time [s]" />
+				</div>
+				<div class="settings__row">
+					<Label for="y-label" class="mb-2 block">Y-axis Label</Label>
+					<Input id="y-label" name="y-label" required placeholder="Amplitude [V]" />
+				</div>
+			</div>
+			
+			{#if numberOfChannels === 2 && channelsToShow !== 1}
+			<div id="ch2-labels">
+				<h4>Channel 2</h4>
+				
+				<div class="settings__row">
+					<Label for="x-label" class="mb-2 block">X-axis Label</Label>
+					<Input id="x-label" name="x-label" required placeholder="Time [s]" />
+				</div>
+				<div class="settings__row">
+					<Label for="y-label" class="mb-2 block">Y-axis Label</Label>
+					<Input id="y-label" name="y-label" required placeholder="Amplitude [V]" />
+				</div>
+			</div>
+			{/if}
+		</section>
+
+		<section class="settings__category">
+			<h3 aria-describedby="helper-legends">Legends</h3>
+			<Helper id="helper-legends">Set potential channel legends</Helper>
+
+			<div class="settings__row">
+				<Label for="ch1-legend" class="mb-2 block">Channel 1</Label>
+				<Input id="ch1-legend" name="ch1-legend" placeholder="Signal 1" />
+			</div>
+			{#if numberOfChannels === 2}
+				<div class="settings__row">
+					<Label for="ch2-legend" class="mb-2 block">Channel 2</Label>
+					<Input id="ch2-legend" name="ch2-legend" placeholder="Signal 2" />
+				</div>
+			{/if}
+		</section>
+		
+		<section class="settings__category">
+			<h3 aria-describedby="helper-presentations">Presentations</h3>
+			<Helper id="helper-presentations">Set optional presentation settings</Helper>
+
+			<Checkbox aria-describedby="helper-checkbox-legends">Show legends</Checkbox>
+			<Helper id="helper-checkbox-legends" class="ps-6">Display legends in figure</Helper>
+			<Checkbox aria-describedby="helper-checkbox-subfigures">Use subfigures</Checkbox>
+			<Helper id="helper-checkbox-subfigures" class="ps-6">Split channels and/or phase plot into subfigures</Helper>
+		</section>
+</section>
 
 <style lang="scss">
-  #banner {
-    padding: 1rem;
-    text-align: center;
-  }
+	#banner {
+		padding: 1rem;
+		text-align: center;
+	}
 
-  #uploader {
-    color: $grey-darken-5;
-    padding: 1rem;
-    text-align: center;
-    display: grid;
-    place-items: center;
-    gap: 1rem;
-    width: 100%;
-  }
+	#uploader {
+		color: $grey-darken-5;
+		padding: 1rem;
+		text-align: center;
+		display: grid;
+		place-items: center;
+		gap: 1rem;
+		width: 100%;
+	}
 
-  #settings {
-    color: $grey-darken-5;
-    padding: 1rem;
-    text-align: center;
+	.plot-type-selector {
+		gap: 1rem;
+		margin-bottom: 1rem;
+	}
 
-    #settings__plot-type {
-      margin-bottom: 1rem;
-    }
+	.fileinfo {
+		text-align: left;
+	}
 
-    #settings__general {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 1rem;
+	.settings {
+		width: 100%;
+		gap: 1rem;
+		display: flex;
 
-      select, input {
-        margin-top: 0.5rem;
-      }
-    }
-  }
+		&__row {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+		}
+	}
 
-  .card {
-    width: 100% !important;
-    display: flex;
-    flex-direction: row-reverse;
-    justify-content: space-between;
-    align-items: center;
-
-    #close {
-      cursor: pointer;
-    }
-
-    .fileinfo {
-      text-align: left;
-    }
-  }
+	.align {
+		&-center {
+			align-items: center;
+		}
+	}
 </style>
